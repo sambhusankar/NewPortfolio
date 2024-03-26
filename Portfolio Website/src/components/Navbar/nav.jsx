89158653c8a1d65002eaa0438734811befbebe1b
sambhusankar <sambhusankarswain@gmail.com>
2024-03-26 12:41:43 +0530

    Navbar is completed

--- Portfolio Website/src/components/Navbar/nav.jsx
+++ Portfolio Website/src/components/Navbar/nav.jsx
@@ -3,40 +3,59 @@ import { Link } from 'react-router-dom'
 import "./nav.css"
 function Nav(){
     const navRef = useRef(null);
-   //copyright rotation 
+    
+   //copyright rotation and logo text change
     useEffect(() => {
         const logo = navRef.current.querySelector(".logo");
         const copyright = navRef.current.querySelector(".copyright");
+        const logo_text = navRef.current.querySelector(".logo-text");
+        let interval1;
+        let interval2;
 
-        logo.addEventListener("mousemove", () => {
+        logo.addEventListener("mouseover", () => {
              copyright.style.transform = "rotate(360deg)"
+
+             clearInterval(interval2)
+             interval1 = setInterval(() => {
+                logo_text.scrollLeft += 1
+             }, 5);
        });
         logo.addEventListener("mouseout", () => {
              copyright.style.transform = "rotate(0deg)"
+
+             clearInterval(interval1)
+             interval2 = setInterval(() => {
+                logo_text.scrollLeft -= 1
+             }, 5);
        });
     }, []);
 
    //dot visible and invisible
     useEffect(() => {
-        const nav_items = navRef.current.querySelectorAll(".nav-item")
-        const dots =  navRef.current.querySelectorAll(".dot")
+        const nav_items = navRef.current.querySelectorAll(".nav-item");
+        const dots =  navRef.current.querySelectorAll(".dot");
+
         for(let i = 0; i < nav_items.length; i++){
             nav_items[i].addEventListener("mousemove", () => {
                 dots[i].style.fontSize = "1vw";
             })
         };
+
         for(let i = 0; i < nav_items.length; i++){
             nav_items[i].addEventListener("mouseout", () => {
                 dots[i].style.fontSize = "0";
             })
         };
-    })
+
+    }, []);
+
+
     
     return(
         <div className= "navbar" ref = {navRef}>
             <Link to = "/" className="logo shakable" >
                 <p className='copyright'>©</p>
-               <p className = "logo-text"> Code By Sankar</p>
+               <p className = "logo-text"> <span>Code by Sankar</span> <span>Sambhu Sankar</span>  </p>
             </Link>
             <Link to = "/work" className="nav-item" >
                 <p className = "shakable">Work</p>
