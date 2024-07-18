e3bf7a33dc92c60718cf18704bfe5a9732c2f25d
Sambhu Sankar <sambhusankarswain@gmail.com>
2024-07-18 21:46:57 +0530

    at the last stage

--- Portfolio Website/src/components/ContactPage/contact.jsx
+++ Portfolio Website/src/components/ContactPage/contact.jsx
@@ -5,17 +5,39 @@ import Footer from "../Footer/footer"
 import Magnetic from "../../Animations/magnetic"
 import "../../Animations/hover-effect.css"
 import "./contact.css"
+import emailjs from "emailjs-com"
 function Contact(){
+    emailjs.init("sikmVpFZ_w8znQeGb")
     const Ref = useRef(null)
+    const form = useRef(null)
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [company, setCompany] = useState('')
     const [service, setService] = useState('')
     const [message, setMessage] = useState('')
 
-    function handleSubmit(){
-        console.log(name)
-    }
+    function handleSubmit(e){
+        
+        let data = {
+            from_name : name,
+            from_email : email,
+            usr_company : company,
+            usr_service : service,
+            message : message
+        }
+        
+        e.preventDefault()
+        emailjs.sendForm("service_own55l5", "template_vev2522", form.current , "sikmVpFZ_w8znQeGb").then(
+            (result) => {
+                console.log(result)
+            }
+    )
+        setName('');
+        setEmail('');
+        setCompany('');
+        setService('');
+        setMessage('');
+}
 
     //setting the page title dynamically
     useEffect(() => {
@@ -36,36 +58,36 @@ function Contact(){
             <div className="Contact-page">
                 <div className="left-side">
                     <h1 className="heading">Let's start a project together</h1>
-                    <form className="user-details">
+                    <form className="user-details" ref = { form }>
                         <div className="form-row">
                             <h5>01</h5>
                             <label>What's your name ?</label>
-                            <input type = "text" placeholder="John Doe *" value = {name} onChange = {(e) => setName(e.target.value)}></input>
+                            <input type = "text" placeholder="John Doe *" value = {name} onChange = {(e) => setName(e.target.value)} name = "user_name"></input>
                         </div>
                         <div className="form-row">
                             <h5>02</h5>
                             <label>What's your email ?</label>
-                            <input type = "email" placeholder="john@doe.com *" value = {email} onChange = {(e) => setEmail(e.target.value)}></input>
+                            <input type = "email" placeholder="john@doe.com *" value = {email} onChange = {(e) => setEmail(e.target.value)} name = "user_email"></input>
                         </div>
                         <div className="form-row">
                             <h5>03</h5>
                             <label>What's the name of your organizaion ?</label>
-                            <input type = "text" placeholder="John & Doe®" value = {company} onChange = {(e) => setCompany(e.target.value)}></input>
+                            <input type = "text" placeholder="John & Doe®" value = {company} onChange = {(e) => setCompany(e.target.value)} name = "user_org"></input>
                         </div>
                         <div className="form-row">
                             <h5>04</h5>
                             <label>What service are you looking for ?</label>
-                            <input type = "text" placeholder="Web Development..." value = {service} onChange = {(e) => setService(e.target.value)}></input>
+                            <input type = "text" placeholder="Web Development..." value = {service} onChange = {(e) => setService(e.target.value)} name = "user_service"></input>
                         </div>
                         <div className="form-row">
                             <h5>05</h5>
                             <label>Your message</label>
-                            <input type = "text" placeholder="Hello Sankar can you help me with...*" value = {message} onChange = {(e) => setMessage(e.target.value)}></input>
+                            <input type = "text" placeholder="Hello Sankar can you help me with...*" value = {message} onChange = {(e) => setMessage(e.target.value)} name = "user_message"></input>
                         </div>
                     </form>
                     <div className="line-btn">
                         <hr></hr>
-                        <div className="send-it-btn hover-effect" onClick = {() => handleSubmit()}><span>Send it !</span></div>
+                        <div className="send-it-btn hover-effect" onClick = {handleSubmit}><span>Send it !</span></div>
                     </div>
                 </div>
                 <div className="right-side">
